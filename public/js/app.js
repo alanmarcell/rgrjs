a3ccd57ca14b375d2917b8ca540dd29f2d840429
alanmarcell@Live.com <alanmarcell@Live.com>
2017-02-18 16:32:15 -0200

    webpack configurated

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,14 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
 var Hello = React.createClass({
-    render: function() {
-        return React.createElement("h3", null, "Hello React from JS!!");
+    render() {
+        return React.createElement("h3", null, "Hello ES6!");
+        // return <h3>     Hello Webpack ? </h3>
+
     }
 });
 
 ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
+
+// <Hello/>, document.getElementById('react'));
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: "./public/js/app.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
