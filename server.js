64cb6bed8ce57ce970871643dcbfc7040f55ee5e
alanmarcell@Live.com <alanmarcell@Live.com>
2017-02-18 14:44:00 -0200

    node static files

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from 'express';
 
 var app = express();
 
-app.get('/', (req, res) => res.send('hello World!!!'));
+// app.get('/', (req, res) => res.send('hello World!!!'));
+
+app.use(express.static('public'));
 
 app.listen(3000);
